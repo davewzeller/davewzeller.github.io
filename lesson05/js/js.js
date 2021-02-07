0d27b5e25330cd68c507e44a0ff71284c9636819
davewzeller <zel9003@byui.edu>
2021-02-06 16:48:30 -0800

    missing JavaScript

--- lesson05/js/js.js
+++ lesson05/js/js.js
@@ -1,8 +1,13 @@
+//date finder
 var d = new Date();
 var n = d.toDateString();
+
+
 document.getElementById("date_last_updated").innerHTML = n;
 
-const hambutton = document.querySelector('.ham');
+
+// hambuger button
+const hambutton = document.querySelector('.fa-bars');
 const mainnav = document.querySelector('.navigation')
 
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
@@ -11,4 +16,23 @@ hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive'
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
 
 
+    var d = new Date();
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
   
+    var b = weekday[d.getDay()];
+    
+
+    if(b == weekday[1]) {
+      var x = "block";
+    }
+    else{
+        var x = "none";
+    }
+    document.getElementById("banner").style.display = x;
