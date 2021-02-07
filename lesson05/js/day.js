f0637319a6d7eecc68080847c759449b91720fd8
davewzeller <zel9003@byui.edu>
2021-02-06 23:09:08 -0800

    giving up tonight

--- lesson05/js/day.js
+++ lesson05/js/day.js
@@ -1,19 +1,13 @@
-function banner() {
-    var x = document.getElementById("banner");
-    var weekday = new Array (7);
-    weekday[0]=  "Sunday";
-    weekday[1] = "Monday";
-    weekday[2] = "Tuesday";
-    weekday[3] = "Wednesday";
-    weekday[4] = "Thursday";
-    weekday[5] = "Friday";
-    weekday[6] = "Saturday";
-    var now = new Date();
-    var day = weekday[now.getDay()];
+let todayBanner = new Date();
 
-    if (day === "Friday") {
-        x.style.display = "block";  
-    }  else {
-       x.style.display = "none"; 
-    }
+window.onload = function banner() { 
+
+if (todayBanner.getDay() == 6 ) {
+  document.getElementById("banner").style.display = "block";
+  
 }
+
+else {
+  document.getElementById("banner").style.display = "none";
+}
+ }
