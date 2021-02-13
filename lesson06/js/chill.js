b79f4ca4c405f1d05a621754f2ab6b8e33853366
davewzeller <zel9003@byui.edu>
2021-02-12 18:15:38 -0800

    fine tuning

--- lesson06/js/chill.js
+++ lesson06/js/chill.js
@@ -3,9 +3,12 @@ var windChill = 65;
 var windSpeed = parseFloat(document.getElementById('windSpeed').textContent);
 var temp = parseFloat(document.getElementById('temp').textContent);
 
-
+if (temp <= 50 && windSpeed >3){
 windChill =(0.0817*(3.71*(Math.pow(windSpeed, 0.5))+5.81-0.25*windSpeed)*(temp-91.4)+91.4);
-
+}
+else{
+    windChill = "n/a"
+}
 document.getElementById("windChill").innerHTML = windChill.toFixed(0);
 
 /*document.querySelector('windChill').textContent = windChill;*/
