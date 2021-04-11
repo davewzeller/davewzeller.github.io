64f65afd41096d0875865d81f23b13f0fbd2ed95
davewzeller <zel9003@byui.edu>
2021-04-11 10:55:24 -0700

    take two

--- lesson1/js.js
+++
@@ -1,2 +0,0 @@
-let oLastModif = document.lastModified;
-document.getElementById("date_last_updated").innerHTML = "Last Updated: " + oLastModif;
--- lesson9/js/stormcenter.js
+++
@@ -1,7 +0,0 @@
-function Rating(rating) {
-    document.getElementById("rating").innerHTML = rating;
-}
-
-function thanks() {
-    window.location = "http//google.com"
-  } 
