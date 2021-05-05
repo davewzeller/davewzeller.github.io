398749ffc3d0e926127a0402e872fee782f1389b
davewzeller <zel9003@byui.edu>
2021-05-05 08:52:45 -0700

    hi

---
+++ lesson3/js/date.js
@@ -0,0 +1,6 @@
+document.querySelector("#datemod").textContent= document.lastModified;
+var d = new Date();
+var n = d.getFullYear();
+document.querySelector("#year").textContent = n;
+
+//Thank goodness for the lab, I fought tooth and nail but couldn't get the dates to work properly
