ca9e0300cb5133ca47e406973fa3ffabdf407200
davewzeller <zel9003@byui.edu>
2021-05-20 17:42:27 -0700

    BOM

---
+++ lesson5/js/bom.js
@@ -0,0 +1,26 @@
+
+const list = document.querySelector('ul');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+
+button.onclick = function() {
+  let myItem = input.value;
+  input.value = '';
+
+  const listItem = document.createElement('li');
+  const listText = document.createElement('ul');
+  const listBtn = document.createElement('button');
+
+  listItem.appendChild(listText);
+  listText.textContent = myItem;
+  listItem.appendChild(listBtn);
+  listBtn.textContent = '❌';
+  list.appendChild(listItem);
+
+  listBtn.onclick = function(e) {
+    list.removeChild(listItem);
+  }
+
+  input.focus();
+}
+//learning-area/javascript/apis/document-manipulation/shopping-list-finished.html
