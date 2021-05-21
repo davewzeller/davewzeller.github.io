337b931ce27066bc7f017e37400e874e408e63b3
davewzeller <zel9003@byui.edu>
2021-05-20 20:35:25 -0700

    java the hut lives

--- lesson5/js/date.js
+++ lesson5/js/date.js
@@ -3,7 +3,18 @@ var d = new Date();
 var n = d.getFullYear();
 var m = d.getMonth();
 var day = d.getDate();
+var n = d.getDay()
+
 document.querySelector("#year").textContent = n;
 document.querySelector("#datemod").textContent=m+1 + " / " + day + " / " + n;
+ 
 
-//Thank goodness for the lab, I fought tooth and nail but couldn't get the dates to work properly // document.lastModified;
+
+    if (d.getDay() == 5) {
+    document.querySelector('.banner').style.display = "block";
+    }
+    else {
+        document.querySelector('.banner').style.display = "none";
+    }
+
+//team effort
