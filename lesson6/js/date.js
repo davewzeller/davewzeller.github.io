3f90040ffdbbbc623616626177593e87eeb5846d
davewzeller <zel9003@byui.edu>
2021-05-27 08:38:11 -0700

    fancy colors

---
+++ lesson6/js/date.js
@@ -0,0 +1,20 @@
+
+var d = new Date();
+var n = d.getFullYear();
+var m = d.getMonth();
+var day = d.getDate();
+
+
+document.querySelector("#year").textContent = n;
+document.querySelector("#datemod").textContent=m+1 + " / " + day + " / " + n;
+ 
+
+
+    if (d.getDay() == 5) {
+    document.querySelector('.banner').style.display = "block";
+    }
+    else {
+        document.querySelector('.banner').style.display = "none";
+    }
+
+//team effort
---
+++ lesson6/js/windchill.js
@@ -0,0 +1,16 @@
+
+const temp = document.getElementById("temp").innerHTML;
+const speed = document.getElementById("speed").innerHTML;
+
+
+
+if (temp< 32){
+var windChill= (35.74 + (0.6215 * temp))-(35.75 * Math.pow(speed,0.16)) + (0.4275*temp*Math.pow(speed,0.16));
+
+var windChill= Math.round(windChill);
+document.querySelector("#windChill").textContent = windChill;
+
+}
+else{
+    document.querySelector("#windChill").textContent = 'NA';
+}
