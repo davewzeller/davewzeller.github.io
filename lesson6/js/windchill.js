fdf46458874ae0f1d131ad9b13057435bc2783f3
davewzeller <zel9003@byui.edu>
2021-05-29 07:34:33 -0700

    Corrected if Statement

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -3,11 +3,11 @@ const speed = document.getElementById("speed").innerHTML;
 
 
 
-if (temp < 32) {
+if (temp <= 50 && speed >= 3 ) {
     var windChill = (35.74 + (0.6215 * temp)) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp * Math.pow(speed, 0.16));
 
     var windChill = Math.round(windChill);
-    document.querySelector("#windChill").textContent = windChill;
+    document.querySelector("#windChill").textContent = windChill + '°f' ;
 
 } else {
     document.querySelector("#windChill").textContent = 'NA';
