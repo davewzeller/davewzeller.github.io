56b1c8395fce2877e59d2a3b4407b876ac7d9e90
davewzeller <zel9003@byui.edu>
2021-05-29 07:40:43 -0700

    Tinkering

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,3 +1,8 @@
+document.querySelector("#temp").textContent= -20; 
+document.querySelector("#speed").textContent= 95; 
+document.querySelector("#humidity").textContent= 100; 
+
+
 const temp = document.getElementById("temp").innerHTML;
 const speed = document.getElementById("speed").innerHTML;
 
@@ -7,7 +12,7 @@ if (temp <= 50 && speed >= 3 ) {
     var windChill = (35.74 + (0.6215 * temp)) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp * Math.pow(speed, 0.16));
 
     var windChill = Math.round(windChill);
-    document.querySelector("#windChill").textContent = windChill + '°f' ;
+    document.querySelector("#windChill").textContent = windChill;
 
 } else {
     document.querySelector("#windChill").textContent = 'NA';
