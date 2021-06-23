9acadac55766221b4f990b5a925691cced71aac0
davewzeller <zel9003@byui.edu>
2021-06-23 11:39:26 -0700

    f

--- lesson10/js/fwpreston.js
+++ lesson10/js/fwpreston.js
@@ -1,4 +1,4 @@
-const apiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=666b87a823c8e0400b1ab1648c657442";
+const apiURL = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=666b87a823c8e0400b1ab1648c657442";
 
 
 fetch(apiURL)
