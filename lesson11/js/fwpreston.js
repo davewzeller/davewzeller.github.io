7cff5ef67fe222c0ff8a4c5cb3cbd5514c8999b6
davewzeller <zel9003@byui.edu>
2021-07-01 13:38:31 -0700

    hi

--- lesson11/js/fwpreston.js
+++ lesson11/js/fwpreston.js
@@ -17,12 +17,48 @@ fetch(apiURL)
 
 
 
+    
+const fwURL = "https://api.openweathermap.org/data/2.5/forecast\\?id=560447&units=imperial&appid=666b87a823c8e0400b1ab1648c657442";
 
-    const dayofWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];  
+  
+
+const daysofWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];  
+
+fetch(fwURL)
+  .then((response) => response.json())
+  .then((response) => {
+    console.log(response);
+
+
+    let x = 0;
+    let temp = document.querySelectorAll('#weather #day #temp');
+    let day = document.querySelectorAll('#weather #day #weekday');
+    let pic = document.querySelector('#weather #day #icon');
+    let list = response.list;
+  
+    for (item of list) {
+      if (item.dt_txt.includes("18:00:00")) {
+        let date = new Date(item.dt * 1000);
+
+        temp[x].innerHTML = item.main.temp;
+        day[x].innerHTML =  daysofWeek[date.getDay()];
+        let img = "https://openweathermap.org/w/" + item.weather[0].icon + ".png";
+        pic[x].setAttribute("src", img);
+        pic[x].setAttribute("alt", "weather image");
+        x++;
 
 
 
-    //day1
+
+      }
+    }
+
+  });
+  });
+
+
+
+ /*      //day1
     document.querySelector("#day1").textContent= Math.round(jsObject.list[8].main.temp);  
     
     const imagesrc1 = 'https://openweathermap.org/img/w/' + jsObject.list[8].weather[0].icon + '.png';   // note the concatenation
@@ -73,6 +109,6 @@ fetch(apiURL)
   
 
 
- 
 
-});
+});*/
+ 
