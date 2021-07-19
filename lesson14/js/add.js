df1c14a4635d466cc9908fd6316e87fa8714711f
davewzeller <zel9003@byui.edu>
2021-07-19 13:02:40 -0700

    retry

--- lesson14/js/add.js
+++ lesson14/js/add.js
@@ -1,4 +1,4 @@
-const requestURL = 'https://davewzeller.github.io/lesson14/add.json';
+const requestURL = 'https://byui-cit230.github.io/canvas-referenced/latterhttps://davewzeller.github.io/lesson14/add.json';
 
 
 fetch(requestURL)//copy of homework
@@ -7,26 +7,26 @@ fetch(requestURL)//copy of homework
   })
   .then(function (jsonObject) {//copy of homework
     console.table(jsonObject);  // temporary checking for valid response and data parsing
-    const add = jsonObject['add'];//copy of homework
+    const prophets = jsonObject['prophets'];//copy of homework
 
-    for (let i = 0; i < add.length; i++ ) { //copy of homework
+    for (let i = 0; i < prophets.length; i++ ) { //copy of homework
         let card = document.createElement('section');//copy of homework
         let h2 = document.createElement('h2');//copy of homework
-        
+       
 
-        let disc = document.createElement('p')
-        let address = document.createElement('p')
-        let phone = document.createElement('p')
+       
+        let birthdate = document.createElement('p')
+        let birthplace = document.createElement('p')
         let photo = document.createElement('img')
-        let photo = document.createElement('p')
 
 
-        h2.textContent = add[i].name;//copy of homework
-        disc.textContent = add[i].disc; 
-        address.textContent = add[i].address;//added There
-        phone.textContent = add[i].phone;
-        photo.setAttribute('src', add[i].imageurl);
-        photo.setAttribute('alt' , add[i].name);
+
+        h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;//copy of homework
+       
+        birthdate.textContent = 'Date of Birth: ' + prophets[i].birthdate;//added There
+        birthplace.textContent = 'Place of Birth: ' + prophets[i].birthplace;
+        photo.setAttribute('src', prophets[i].imageurl);
+        photo.setAttribute('alt' , prophets[i].name + '' + prophets[i].lastname +'-'+ i);
        
        
        
@@ -35,9 +35,8 @@ fetch(requestURL)//copy of homework
         card.appendChild(h2);//copy of homework
 
 
-        card.appendChild(disc); //add three
-        card.appendChild(phone);
-        card.appendChild(address);
+        card.appendChild(birthdate); //add three
+        card.appendChild(birthplace);
         card.appendChild(photo);
 
 
